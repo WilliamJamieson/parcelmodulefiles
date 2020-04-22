7aaba1d608b81099d9ab77e3ec54b33de27d5717
William Jamieson <william.b.jamieson@nasa.gov>
2020-04-22 16:42:45 -0400

    Created Standard environment file

---
+++ StdEnv.lua
@@ -0,0 +1,6 @@
+-- quick load standard env --
+
+load("Core/MKL/2020.0.166")
+load("Core/gcc-gfortran/9.3.0")
+load("Compiler/gcc-gfortran-9.3.0/openmpi/4.0.3")
+load("MPI/gcc-gfortran-9.3.0/openmpi-4.0.3/Baselibs/6.0.11")
