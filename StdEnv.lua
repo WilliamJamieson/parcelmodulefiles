fa5b873df79985efa721d2085a00e1b88331db6b
William Jamieson <william.b.jamieson@nasa.gov>
2020-09-17 14:33:37 -0400

    Updated StdEnv to the latest

--- StdEnv.lua
+++ StdEnv.lua
@@ -2,5 +2,5 @@
 
 load("Core/MKL/2020.0.166")
 load("Core/gcc-gfortran/10.2.0")
-load("Compiler/gcc-gfortran-10.2.0/openmpi/4.0.4")
-load("MPI/gcc-gfortran-10.2.0/openmpi-4.0.4/Baselibs/6.0.16")
+load("Compiler/gcc-gfortran-10.2.0/openmpi/4.0.5")
+load("MPI/gcc-gfortran-10.2.0/openmpi-4.0.5/Baselibs/6.0.16")
