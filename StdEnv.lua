a9676a6830cfe75e2561a2759d5de0f90f509ad7
William Jamieson <william.b.jamieson@nasa.gov>
2020-12-01 12:38:30 -0500

    Added Support for GEOSpyD

--- StdEnv.lua
+++ StdEnv.lua
@@ -1,6 +1,7 @@
 -- quick load standard env --
 
 load("Core/MKL/2020.0.166")
+load("Core/GEOSpyD/Min4.8.3_py2.7")
 load("Core/gcc-gfortran/10.2.0")
 load("Compiler/gcc-gfortran-10.2.0/openmpi/4.0.5")
 load("MPI/gcc-gfortran-10.2.0/openmpi-4.0.5/Baselibs/6.0.16")
