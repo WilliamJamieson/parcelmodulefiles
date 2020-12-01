e0f1eef7d153c1436dcfc9dad8a3b6c0f76897cf
William Jamieson <william.b.jamieson@nasa.gov>
2020-12-01 12:18:36 -0500

    Added some more standard environments

---
+++ StdEnv_4.0.4.lua
@@ -0,0 +1,6 @@
+-- quick load standard env --
+
+load("Core/MKL/2020.0.166")
+load("Core/gcc-gfortran/10.2.0")
+load("Compiler/gcc-gfortran-10.2.0/openmpi/4.0.4")
+load("MPI/gcc-gfortran-10.2.0/openmpi-4.0.4/Baselibs/6.0.16")
